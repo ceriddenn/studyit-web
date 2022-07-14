aca875a7ce282f61fdd6344bfc9b1ad40a583303
ceriddenn <ceriddenn@gmail.com>
2022-07-14 04:29:51 -0400

    fix

---
+++ pages/dashboard/study/quickstudy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const quickstudy = () => {
+  return (
+    <div>quickstudy (NOT DONE)</div>
+  )
+}
+
+export default quickstudy
---
+++ pages/dashboard/study/quizes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const quizes = () => {
+  return (
+    <div>quizes page (NOT DONE)</div>
+  )
+}
+
+export default quizes
