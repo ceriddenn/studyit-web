3fef0a709c2cca15fbbaeac59354d1617e2a01a2
ceriddenn <ceriddenn@gmail.com>
2022-08-05 14:38:17 -0400

    fixed another few things NOW DEPLOY!!!

--- components/EditDeck.js
+++ components/EditDeck.js
@@ -93,28 +93,28 @@ const initiateEdit = async (event) => {
   return (
     <div class="flex justify-center overflow-y-auto overflow-x-hidden absolute top-0 right-0 left-0 bottom-0">
     <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-800">
+        <div class="relative rounded-lg shadow bg-gray-800">
             <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" onClick={() => window.location.reload()}>
                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
             </button>
             <div class="py-6 px-6 lg:px-8">
-                <h3 class="mb-2 text-xl font-medium text-gray-900 dark:text-white">Edit a Deck</h3>
+                <h3 class="mb-2 text-xl font-medium text-white">Edit a Deck</h3>
                 <h1 class="mb-2 text-red-500 font-bold">{error !== null && error}</h1>
                 <form class="space-y-6 " onSubmit={event => initiateEdit(event)}>
                 {deckData.map(d => (
                   <div>
                     <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Deck Name</label>
-                        <input type="text" id="deck-name" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder={d.deckName} onChange={event => handleChangeInput1(d.deckId, event.target.value, "deckName")}/>
+                        <label class="block mb-2 text-sm font-medium text-gray-300">Deck Name</label>
+                        <input type="text" id="deck-name" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder={d.deckName} onChange={event => handleChangeInput1(d.deckId, event.target.value, "deckName")}/>
                     </div>
                     <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Deck Description</label>
-                        <input type="text" id="deck-description" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white" placeholder={d.description} onChange={event => handleChangeInput1(d.deckId, event.target.value, "description")}/>
+                        <label class="block mb-2 text-sm font-medium text-gray-300">Deck Description</label>
+                        <input type="text" id="deck-description" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 text-white" placeholder={d.description} onChange={event => handleChangeInput1(d.deckId, event.target.value, "description")}/>
                     </div>
     
                     <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Deck Owner</label>
-                        <input type="text" placeholder={session.user.id.slice(0, -12) + "************"} class="bg-gray-900 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-500 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" disabled/>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-300">Deck Owner</label>
+                        <input type="text" placeholder={session.user.id.slice(0, -12) + "************"} class="bg-gray-900 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-500 border-gray-500 placeholder-gray-400 text-white" disabled/>
                     </div>
                     </div>))}
                     <div class="flex flex-row">
@@ -126,12 +126,12 @@ const initiateEdit = async (event) => {
                     {deckContents.map(ii => (
                     <div class="flex justify-between gap-3" key={ii.id}>
                     <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Term</label>
-                        <input type="text" id="deck-description" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white" placeholder={ii.term} onChange={event => handleChangeInput(ii.id, event.target.value, "term")}/>
+                        <label class="block mb-2 text-sm font-medium text-gray-300">Term</label>
+                        <input type="text" id="deck-description" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 text-white" placeholder={ii.term} onChange={event => handleChangeInput(ii.id, event.target.value, "term")}/>
                     </div>
                     <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Definition</label>
-                        <input type="text" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white" placeholder={ii.definition} onChange={event => handleChangeInput(ii.id, event.target.value, "definition")}/>
+                        <label class="block mb-2 text-sm font-medium text-gray-300">Definition</label>
+                        <input type="text" class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 text-white" placeholder={ii.definition} onChange={event => handleChangeInput(ii.id, event.target.value, "definition")}/>
                     </div>
                     <div class="pl-2 cursor-pointer flex flex-row pt-8" onClick={() => handleRemoveInput(ii.id)}>
                     <MinusIcon class="text-white h-7 w-7 px-1 bg-gray-600 rounded-full cursor-pointer transition ease-in-out hover:scale-110"/>
